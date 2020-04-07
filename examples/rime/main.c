a83626fe339f93422bd88eabf48d475915e78c1e
Robert Olsson <robert@herjulf.se>
2020-04-07 10:05:09 +0200

    Cleanup main.c

--- examples/rime/main.c
+++ examples/rime/main.c
@@ -1,7 +1,5 @@
 /*
- * Copyright (C) tik2008, 2009, 2010 Kaspar Schleiser <kaspar@schleiser.de>
- * Copyright (C) 2013 INRIA
- * Copyright (C) 2013 Ludwig Knüpfer <ludwig.knuepfer@fu-berlin.de>
+ * Copyright (C) 2020 Robert Olsson
  *
  * This file is subject to the terms and conditions of the GNU Lesser
  * General Public License v2.1. See the file LICENSE in the top level
@@ -13,11 +11,9 @@
  * @{
  *
  * @file
- * @brief       Default application that shows a lot of functionality of RIOT
+ * @brief       rime application
  *
- * @author      Kaspar Schleiser <kaspar@schleiser.de>
- * @author      Oliver Hahm <oliver.hahm@inria.fr>
- * @author      Ludwig Knüpfer <ludwig.knuepfer@fu-berlin.de>
+ * @author      Robert Olsson <roolss@kth.se>
  *
  * @}
  */
@@ -27,9 +23,6 @@
 #include "thread.h"
 #include "xtimer.h"
 #include "timex.h"
-#include "shell.h"
-#include "shell_commands.h"
-#include "od.h"
 
 #ifdef MODULE_NETIF
 #include "net/gnrc/pktdump.h"
@@ -113,58 +106,6 @@ void print_report(gnrc_pktsnip_t *pkt)
 #endif
 }
 
-static void _dump_snip(gnrc_pktsnip_t *pkt)
-{
-    size_t hdr_len = pkt->size;
-    
-    switch (pkt->type) {
-        case GNRC_NETTYPE_UNDEF:
-            printf("NETTYPE_UNDEF (%i)\n", pkt->type);
-            od_hex_dump(pkt->data, pkt->size, OD_WIDTH_DEFAULT);
-            break;
-#ifdef MODULE_GNRC_NETIF
-    case GNRC_NETTYPE_NETIF:
-            printf("NETTYPE_NETIF (%i)\n", pkt->type);
-            od_hex_dump(pkt->data, pkt->size, OD_WIDTH_DEFAULT);	    
-            gnrc_netif_hdr_print(pkt->data);
-            break;
-#endif
-        default:
-            printf("NETTYPE_UNKNOWN (%i)\n", pkt->type);
-            od_hex_dump(pkt->data, pkt->size, OD_WIDTH_DEFAULT);
-            break;
-    }
-    if (hdr_len < pkt->size) {
-        size_t size = pkt->size - hdr_len;
-
-        od_hex_dump(((uint8_t *)pkt->data) + hdr_len, size, OD_WIDTH_DEFAULT);
-    }
-}
-
-void __rime_msg(gnrc_pktsnip_t *pkt)
-{
-    int snips = 0;
-    int size = 0;
-    gnrc_pktsnip_t *snip = pkt;
-    count++;
-#if 0
-    printf("pkts=%lu\n", count);
-    gnrc_pktbuf_release(pkt);
-    return;
-#endif    
-    while (snip != NULL) {
-        printf("~~ SNIP %2i - size: %3u byte, type: ", snips,
-               (unsigned int)snip->size);
-        _dump_snip(snip);
-        ++snips;
-        size += snip->size;
-        snip = snip->next;
-    }
-
-    printf("~~ PKT    - %2i snips, total size: %3i byte\n", snips, size);
-    gnrc_pktbuf_release(pkt);
-}
-
 void rime_msg(gnrc_pktsnip_t *pkt)
 {
   gnrc_pktsnip_t *snip = pkt;
@@ -179,9 +120,6 @@ void rime_msg(gnrc_pktsnip_t *pkt)
   
   gnrc_pktbuf_release(snip);
 }
-
-// 00000000  81  00  38  5A  1F  9A  54  58  54  3D  56  5F  52  55  4D  20
-// 00000000  1F  36  05  34  32  30  01  D9  00  00  00  00  00  00  01
 
 static int rime_send(gnrc_netif_t *iface, struct payload* payload, int payload_len)
 {
@@ -206,7 +144,6 @@ static int rime_send(gnrc_netif_t *iface, struct payload* payload, int payload_l
     l2addr[0] = iface->l2addr[7];
     l2addr[1] = iface->l2addr[6];
 
-    //res = gnrc_netapi_set(iface->pid, NETOPT_ADDRESS,0, &iface->l2addr, sizeof(src_len));
     res = gnrc_netapi_set(iface->pid, NETOPT_ADDRESS,0, &l2addr, sizeof(src_len));
     
     if(res < 0 )
@@ -221,14 +158,10 @@ static int rime_send(gnrc_netif_t *iface, struct payload* payload, int payload_l
     hdr->flags |= GNRC_NETIF_HDR_FLAGS_BROADCAST;
     LL_PREPEND(pkt, netif);
     gnrc_netif_send(iface, pkt);
-
     return 1;
 }
 
-unsigned char epc[13] = {0x34, 0x32, 0x30, 0x01, 0xD9, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01};
 char *epc_str= "34323001D900000000000001";
-
-//int8_t seqno = 0; 
 
 int len_exceeded(int len)
 {
@@ -262,10 +195,10 @@ void send_sensd_pkt(gnrc_netif_t *iface)
     if( len_exceeded(len)) 
       return;
   }
-  msg.channel = 0x81; /* 129 */
+  msg.channel = 0x81; /* 129 for Contiki */
   msg.dummy[0] = 0xAB;
   msg.dummy[1] = 0xBA;
-  msg.head = (1<<4); /* Version 1 */
+  msg.head = (1<<4); /* Version 1 for sensd */
   msg.head |= ttl;
   msg.seqno = seqno++;
   msg.buf[len++] = 0;
@@ -274,8 +207,6 @@ void send_sensd_pkt(gnrc_netif_t *iface)
 
 int main(void)
 {
-  uint32_t pkt_us;
-  int cnt;
   int res;
 
   xtimer_ticks32_t last_wakeup = xtimer_now();
@@ -285,9 +216,6 @@ int main(void)
   gnrc_netreg_register(GNRC_NETTYPE_UNDEF, &dump);
 #endif
     
-  //char line_buf[SHELL_DEFAULT_BUFSIZE];
-  //shell_run(NULL, line_buf, SHELL_DEFAULT_BUFSIZE);
-
   gnrc_pktdump_init();
   gnrc_netif_t *iface = gnrc_netif_get_by_pid(4);
 
@@ -301,29 +229,8 @@ int main(void)
   if (res < 0)
     printf("Setting Chan failed\n");
   
-  cnt = COUNT;
-
   while(1) {
-    uint32_t t1, t2, pps;
-    uint8_t ttl = DEF_TTL;
     xtimer_periodic_wakeup(&last_wakeup, INTERVAL);
-    
-    t1 = xtimer_usec_from_ticks(xtimer_now());
-
-    while(0 && cnt && cnt--) {
-      p.head = (1<<4); /* Version 1 */
-      p.head |= ttl;
-      p.seqno++;
-      memcpy(p.buf, epc, 12); 
-      rime_send(iface, &p, sizeof(p));
-    }
-
     send_sensd_pkt(iface);
-    
-    t2 = xtimer_usec_from_ticks(xtimer_now());
-    cnt = COUNT;
-    pkt_us = (t2-t1)/COUNT;
-    pps = 1000000/pkt_us;
-    //printf("sent=%d packet_us=%lu pps=%lu\n", cnt, pkt_us, pps);
   }
 }
